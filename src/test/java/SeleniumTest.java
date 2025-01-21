abd0f0583da7dfbd8c963105de4a39ecce0fb20a
Jeremy Opriasa <opriasajbusiness@gmail.com>
2025-01-21 21:58:10 +0800

    feat: Selenium Java level 1

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,3 +1,14 @@
-public class SeleniumTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+public class SeleniumTest {
+    @Test
+    void Setup(){
+
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opriasaweb.github.io/opriasa-de-portfolio/index.html");
+
+    }
 }
